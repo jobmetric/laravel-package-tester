452096d2f3b7acef7c8ab6dc518db789aea48d84
matin <themainmatin@gmail.com>
2026-02-09 21:52:54 +0330

    changed service provider

--- src/PackageTesterServiceProvider.php
+++ src/PackageTesterServiceProvider.php
@@ -2,42 +2,23 @@
 
 namespace JobMetric\PackageTester;
 
-use Illuminate\Support\ServiceProvider;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
 
-abstract class PackageTesterServiceProvider extends ServiceProvider
+class PackageTesterServiceProvider extends PackageCoreServiceProvider
 {
     /**
-     * register provider
+     * set configuration package
      *
+     * @param PackageCore $package
      * @return void
      */
-    public function register(): void
+    public function configuration(PackageCore $package): void
     {
-        $basePath = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..');
-
-        // register config
-        $this->mergeConfigFrom($basePath . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php', 'package-tester');
+        $package->name = 'laravel-package-tester';
+        $package->option['hasConfig'] = true;
+        $package->option['commands'] = [
+            \JobMetric\PackageTester\Commands\RunTesterCommand::class,
+        ];
     }
-
-    /**
-     * boot provider
-     *
-     * @return void
-     */
-    public function boot(): void
-    {
-        $basePath = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..');
-
-        // publish config
-        $this->publishes([
-            $basePath . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php' => config_path('package-tester.php'),
-        ], 'config');
-
-        // load commands
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                \JobMetric\PackageTester\Commands\PackageTesterCommand::class,
-            ]);
-        }
-    }
-}
+}   
