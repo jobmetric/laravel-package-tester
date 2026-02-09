7a24fc407f4955ded73de2f36b8c352f98e232c5
majidmohammadian <majeedmohammadian@gmail.com>
2026-02-09 22:55:28 +0330

    changed PackageTesterServiceProvider based on package core

--- src/PackageTesterServiceProvider.php
+++ src/PackageTesterServiceProvider.php
@@ -11,14 +11,12 @@ class PackageTesterServiceProvider extends PackageCoreServiceProvider
      * set configuration package
      *
      * @param PackageCore $package
+     *
      * @return void
      */
     public function configuration(PackageCore $package): void
     {
-        $package->name = 'laravel-package-tester';
-        $package->option['hasConfig'] = true;
-        $package->option['commands'] = [
-            \JobMetric\PackageTester\Commands\RunTesterCommand::class,
-        ];
+        $package->name('laravel-package-tester')
+            ->registerCommand(Commands\RunTesterCommand::class);
     }
 }   
